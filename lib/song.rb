fd903fc70358b35926b6827a5aa9f96fee73965c
Garrett <Garrett.Bodley@gmail.com>
2020-11-03 13:45:19 -0500

    commit tree 2b8e73a711ec77b3978a2055e22b0f15e200c22a

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,58 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def initialize(name=nil, artist_name=nil)
+    @name = name
+    @artist_name = artist_name
+  end
+
   def self.all
     @@all
   end
 
   def save
     self.class.all << self
+    self
+  end
+
+  def self.create
+    song = self.new
+    song.save
+  end
+
+  def self.new_by_name(name)
+    song = self.new(name)
+  end
+
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    song.save
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|song| name == song.name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) ? self.find_by_name(name) : self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    filename = filename.split(/.mp3| - /)
+    self.new(filename[1], filename[0])
+  end
+
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+    song.save
+  end
+
+  def self.destroy_all
+    self.all.clear
   end
 
 end
